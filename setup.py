0a2e14b95d1130768d8d93bab2593e2ea9fa9047
Hiroaki KAWAI <hiroaki.kawai@gmail.com>
2020-08-11 12:27:30 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from setuptools import setup, find_packages
 
+with open("README.md","r") as fh:
+	long_description = fh.read()
+
 setup(name='osara',
 	version='0.0.1',
 	description='micro-framewaork on top of kappa architecture',
 	author='Hiroaki Kawai',
+	author_email="hiroaki.kawai@gmail.com",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
 	url='http://github.com/hkwi/osara',
 	packages=find_packages(exclude=("tests",)),
 	install_requires=[
@@ -11,4 +17,10 @@ setup(name='osara',
 		'sqlalchemy',
 		'werkzeug',
 	],
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires=">=3.6",
 )
